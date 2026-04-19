4b0b631ddab5f60cbdad6f4553797763aa9b5c3f
Nieressa <Nieressa@KIMMACLUP>
2026-04-19 21:45:52 +0800

    AppServiceProvider

---
+++ routes/AppServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+namespace App\Providers;
+
+
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\URL;
+
+
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    /**
+
+     * Register any application services.
+
+     */
+
+    public function register(): void
+    {
+
+        //
+
+    }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
+    public function boot(): void
+    {
+
+        if (env('APP_ENV') === 'ems_db') {
+
+            URL::forceScheme('https');
+
+        }
+
+    }
+
+}
